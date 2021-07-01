622e6256dab84fe5ab50db40be2497de674c5705
Lane2002 <woodelaine2002@hotmail.co.uk>
2021-07-01 19:02:40 +0100

    first commit

---
+++ files/Tree.java
@@ -0,0 +1,13 @@
+// Define your tree class in this file!
+class Tree{
+    public static void main(String[] args){
+      //I'm going to print a tree to the screen
+      System.out.println("Hey there, I’m Ariel and I’m learning to code in Java!");
+      System.out.println("I’m going to plant a tree today");
+      System.out.println("  *  ");
+     System.out.println(" *** ");
+     System.out.println(" *** ");
+     System.out.println("  *  ");
+     System.out.println("  *  ");  
+    }
+ }
